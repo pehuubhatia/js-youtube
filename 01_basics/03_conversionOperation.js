69e308e1b861603766bb23291ef3a96b0191f69e
pehuubhatia <129961294+pehuubhatia@users.noreply.github.com>
2025-01-01 11:54:07 +0000

    conversion of operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+// let score = "33abc"
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)//NaN
+
+// let score = null
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)//0
+
+// let score = undefined
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)//NaN
+
+// let score = true
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)//1
+
+// let score = "pehu"
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)//NaN
+
+// let isLoggedIn = "pehu"
+// let boolIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolIsLoggedIn)
+
+//1 - true; 0- false
+//" "- false
+// "pehu"- true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
