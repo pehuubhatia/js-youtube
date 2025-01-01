ee36ad37c3964fec86bed8e2853c81416c5aec41
pehuubhatia <129961294+pehuubhatia@users.noreply.github.com>
2025-01-01 10:49:58 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+ "use strict" ; //treat all js code as newer version
+ //alert(3+3) we are using nodejs not browser
+ //code readability should be high
+ let name = "pehu"
+ let age = 20
+ let loggedin = true
+ let state;
+ /*
+ datatypes:
+ number - 2 to the power 53
+ bigint
+ string- " "
+ boolean
+ null- standalone value
+ undefined - value not yet assigned
+ symbol- unique
+ object
+*/
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+
